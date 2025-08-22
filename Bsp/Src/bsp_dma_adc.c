af6a5e06e8fa7b14120c5e42d85fa625b5c08672
haliwt <jonewang@126.com>
2025-08-22 19:25:25 +0800

    read ADC voltage is success .

--- Bsp/Src/bsp_dma_adc.c
+++ Bsp/Src/bsp_dma_adc.c
@@ -21,48 +21,36 @@ adc_typedef_t adc_t;
 
 /**
  * 
- * @brief
+ * @brief: read ADC switch result voltage value 
  * @note:
  * @parameter :
  */
 void adcRead_voltageValue(void)
  {
-    uint8_t i;
+    /* 打印采样结果 */
+
+     adc_t.adc_detected= (adc_buf[0]  * 3300)/4095;
+     adc_t.adc_temperature = (adc_buf[1] * 3300)/4095;
+     adc_t.adc_atomization =  (adc_buf[2] * 3300)/4095;
+
+     printf("adc_det = %d\r\n",adc_t.adc_detected);
+     printf("adc_temp = %d\r\n",adc_t.adc_temperature);
+     printf("adc_atom = %d\r\n",adc_t.adc_atomization);
+     
+      adc_again_enable_adc();
+
     
-    if(dma_trans_complete_flag==1){
-        dma_trans_complete_flag ++;
 
-          /* 打印采样结果 */
-    for(i = 0; i < ADC_CHANNEL_COUNT; i++)
-    {
-        voltage = (adc_buf[i] / 4095) * 3300;
-
-        printf("CH[%d] ADC=%d  Voltage=%d V\r\n", i, adc_buf[i], voltage);
-
-    }
-
-   }
-
-   if( dma_trans_complete_flag ==2){
-
-      dma_trans_complete_flag =0;
-        // 清除 DMA 完成标志
-      dma_flag_clear(DMA1_FDT1_FLAG);
-    /* flexible function enable */
-      dma_flexible_config(DMA1, FLEX_CHANNEL3, DMA_FLEXIBLE_ADC1);
-    // 重新打开 DMA 通道
-    dma_channel_enable(DMA1_CHANNEL1, TRUE);
-
-    // 软件触发 ADC 转换
-    adc_ordinary_software_trigger_enable(ADC1, TRUE);
+}
 
    
-   }
 
-
- }
-
-
+/**
+ * 
+ * @brief: read ADC switch result voltage value 
+ * @note:
+ * @parameter :
+ */
  static void adc_again_enable_adc(void)
  {
  adc_base_config_type adc_base_struct;
